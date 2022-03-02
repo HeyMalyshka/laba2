135f18e312c44abfa4b49de35c2fe830eec8b6d6
HeyMalyshka <ilonkatop987@gmail.com>
2022-03-02 15:07:33 +0500

    gfvr

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            else if (c == '-') s = a - b;
+         
+
+
         }
     }
 }
