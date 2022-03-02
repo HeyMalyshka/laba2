4a24bb96dc80d528a6aabdfbf128d922ddbf564f
HeyMalyshka <ilonkatop987@gmail.com>
2022-03-02 16:20:19 +0500

    hii

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,22 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("vvedite A");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("vvedite B");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("cho sdelat: *, /, +, -");
+            char c = Convert.ToChar(Console.ReadLine());
+            
+            int s=0;
+            if (c == '+') s = a + b;
+            else if (c == '-') s = a - b;
+            else if (c == '*') s = a * b;
+            else if (c == '/') s = a / b;
+
+            Console.WriteLine("otvet = ", s);
         }
     }
 }
